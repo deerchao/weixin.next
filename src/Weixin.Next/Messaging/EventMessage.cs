94173693a6db0134c12db1420d77b599e41231dd
陆超 <deerchao@live.com>
2016-08-11 18:36:29 +0800

    添加加解密代码；添加大部分请求消息类

---
+++ src/Weixin.Next/Messaging/EventMessage.cs
@@ -0,0 +1,105 @@
+﻿namespace Weixin.Next.Messaging
+{
+    public abstract class EventMessage : RequestMessage
+    {
+        /// <summary>
+        /// 事件类型
+        /// </summary>
+        public string Event { get; set; }
+    }
+
+    public abstract class KeyedEventMessage : EventMessage
+    {
+        /// <summary>
+        /// 在不同子类中, 代表不同的意义
+        /// </summary>
+        public string EventKey { get; set; }
+    }
+
+    public abstract class TicketedEventMessage : KeyedEventMessage
+    {
+        /// <summary>
+        /// 在不同子类中, 代表不同的意义
+        /// </summary>
+        public string Ticket { get; set; }
+    }
+
+
+    /// <summary>
+    /// subscribe 订阅
+    /// <para>
+    /// EventKey: 只有通过扫描带参数二维码关注时才有此字段，qrscene_为前缀，后面为二维码的参数值
+    /// <br/>
+    /// Ticket: 只有通过扫描带参数二维码关注时才有此字段，二维码的ticket，可用来换取二维码图片
+    /// </para>
+    /// </summary>
+    public class SubscribeEventMessage : TicketedEventMessage
+    {
+        /// <summary>
+        /// 获取扫描的二维码参数值
+        /// </summary>
+        /// <returns></returns>
+        public string GetQrSceneValue()
+        {
+            return EventKey?.Substring("qrscene_".Length);
+        }
+    }
+
+    /// <summary>
+    /// unsubscribe 取消订阅
+    /// </summary>
+    public class UnsubscribeEventMessage : EventMessage
+    {
+    }
+
+    /// <summary>
+    /// scan 扫描带参数二维码
+    /// <para>
+    /// EventKey: 一个32位无符号整数，即创建二维码时的二维码scene_id
+    /// <br/>
+    /// Ticket: 二维码的ticket，可用来换取二维码图片
+    /// </para>
+    /// </summary>
+    public class ScanEventMessage : TicketedEventMessage
+    {
+    }
+
+    /// <summary>
+    /// location 上报地理位置
+    /// </summary>
+    public class LocationEventMessage : EventMessage
+    {
+        /// <summary>
+        /// 地理位置纬度
+        /// </summary>
+        public double Latitude { get; set; }
+        /// <summary>
+        /// 地理位置经度
+        /// </summary>
+        public double Longitude { get; set; }
+        /// <summary>
+        /// 地理位置精度
+        /// </summary>
+        public double Precision { get; set; }
+    }
+
+    /// <summary>
+    /// click 点击菜单拉取消息
+    /// <para>
+    /// EventKey: 与自定义菜单接口中KEY值对应
+    /// </para>
+    /// </summary>
+    public class ClickEventMessage : KeyedEventMessage
+    {
+    }
+
+    /// <summary>
+    /// view 点击菜单拉取消息
+    /// <para>
+    /// EventKey: 设置的跳转URL
+    /// </para>
+    /// </summary>
+    public class ViewEventMessage : KeyedEventMessage
+    {
+    }
+}
---
+++ src/Weixin.Next/Messaging/RequestMessage.cs
@@ -0,0 +1,147 @@
+﻿namespace Weixin.Next.Messaging
+{
+    public abstract class RequestMessage
+    {
+        /// <summary>
+        /// 开发者微信号
+        /// </summary>
+        public string ToUserName { get; set; }
+        /// <summary>
+        /// 发送方帐号（一个OpenID）
+        /// </summary>
+        public string FromUserName { get; set; }
+        /// <summary>
+        /// 消息创建时间 （时间戳）
+        /// </summary>
+        public long CreateTime { get; set; }
+        /// <summary>
+        /// 消息类型
+        /// </summary>
+        public string MsgType { get; set; }
+        /// <summary>
+        /// 消息id，64位整型
+        /// </summary>
+        public long MsgId { get; set; }
+    }
+
+
+    /// <summary>
+    /// text 文本消息
+    /// </summary>
+    public class TextRequestMessage : RequestMessage
+    {
+        /// <summary>
+        /// 消息内容
+        /// </summary>
+        public string Content { get; set; }
+    }
+
+    /// <summary>
+    /// image 图片消息
+    /// </summary>
+    public class ImageRequestMessage : RequestMessage
+    {
+        /// <summary>
+        /// 图片链接（由系统生成）
+        /// </summary>
+        public string PicUrl { get; set; }
+
+        /// <summary>
+        /// 媒体id，可以调用多媒体文件下载接口拉取数据。
+        /// </summary>
+        public string MediaId { get; set; }
+    }
+
+    /// <summary>
+    /// voice 语音消息
+    /// </summary>
+    public class VoiceRequestMessage : RequestMessage
+    {
+        /// <summary>
+        /// 媒体id，可以调用多媒体文件下载接口拉取数据。
+        /// </summary>
+        public string MediaId { get; set; }
+        /// <summary>
+        /// 语音格式，如amr，speex等
+        /// </summary>
+        public string Format { get; set; }
+        /// <summary>
+        /// 开通语音识别后才有, 语音识别结果
+        /// </summary>
+        public string Recognition { get; set; }
+    }
+
+    /// <summary>
+    /// video 视频消息
+    /// </summary>
+    public class VideoRequestMessage : RequestMessage
+    {
+        /// <summary>
+        /// 媒体id，可以调用多媒体文件下载接口拉取数据。
+        /// </summary>
+        public string MediaId { get; set; }
+        /// <summary>
+        /// 视频消息缩略图的媒体id，可以调用多媒体文件下载接口拉取数
+        /// </summary>
+        public string ThumbMediaId { get; set; }
+    }
+
+    /// <summary>
+    /// shortvideo 小视频消息
+    /// </summary>
+    public class ShortVideoRequestMessage : RequestMessage
+    {
+        /// <summary>
+        /// 媒体id，可以调用多媒体文件下载接口拉取数据。
+        /// </summary>
+        public string MediaId { get; set; }
+        /// <summary>
+        /// 视频消息缩略图的媒体id，可以调用多媒体文件下载接口拉取数
+        /// </summary>
+        public string ThumbMediaId { get; set; }
+    }
+
+    /// <summary>
+    /// location 地理位置消息
+    /// </summary>
+    public class LocationRequstMessage : RequestMessage
+    {
+        /// <summary>
+        /// 地理位置纬度
+        /// </summary>
+        public double Location_X { get; set; }
+        /// <summary>
+        /// 地理位置经度
+        /// </summary>
+        public double Location_Y { get; set; }
+        /// <summary>
+        /// 地图缩放大小
+        /// </summary>
+        public int Scale { get; set; }
+        /// <summary>
+        /// 地理位置信息
+        /// </summary>
+        public string Label { get; set; }
+    }
+
+    /// <summary>
+    /// link 链接消息
+    /// </summary>
+    public class LinkRequestMessage : RequestMessage
+    {
+
+        /// <summary>
+        /// 消息标题
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// 消息描述
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// 消息链接
+        /// </summary>
+        public string Url { get; set; }
+    }
+
+}
