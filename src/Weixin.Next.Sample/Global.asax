5448e96dcbb2ebe751d3943b869275cb6194574c
陆超 <deerchao@live.com>
2016-08-16 15:45:34 +0800

    添加 Sample 项目

---
+++ src/Weixin.Next.Sample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Weixin.Next.Sample.MvcApplication" Language="C#" %>
---
+++ src/Weixin.Next.Sample/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Weixin.Next.Sample")]
+[assembly: AssemblyDescription("Sample application for Weixin.Next")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("deerchao")]
+[assembly: AssemblyProduct("Weixin.Next.Sample")]
+[assembly: AssemblyCopyright("Copyright © deerchao 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f497552a-e818-41ef-921e-72e99b09865b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.*")]
